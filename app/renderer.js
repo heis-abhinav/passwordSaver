54ac0e7da93b01f74ef9c918e6ef6660d910a481
Abhinav <abhinavkumr77@gmail.com>
2022-05-17 19:33:28 +0530

    update view dynamically

--- app/renderer.js
+++ app/renderer.js
@@ -3,6 +3,7 @@ const {ipcRenderer} = require('electron');
 const mainProcess = require('@electron/remote').require('./main.js');
 const currentWindow = require('@electron/remote').getCurrentWindow();
 const addButton = document.querySelector('.add-btn');
+const vault = new Set();
 
 closeBtn.addEventListener('click', () =>{
 	mainProcess.closeWindow(currentWindow);
@@ -23,14 +24,30 @@ const createPasswordElement = (name, username, password) => {
 };
 
 
-addButton.addEventListener('click', ()=> {
+addButton.addEventListener('click', () => {
 	/*const passwordElement = createPasswordElement('abhinav', 'a', 'b');
 	PasswordList.prepend(passwordElement);*/
 	mainProcess.addCredentialsBox(currentWindow);
 });
 
 ipcRenderer.on('this-message', (event, message) => {
-	console.log(typeof(message))
-	const passwordElement = createPasswordElement(...Object.values(message));
-	PasswordList.prepend(passwordElement);
+	vault.add(message)
+	PasswordList.innerHTML = '';
+	updateRenderer(vault);
+	//console.log([...vault][0])
+	/*const passwordElement = createPasswordElement(...Object.values(message));
+	PasswordList.prepend(passwordElement);*/
 })
+
+
+const updateRenderer = (vault) => {
+	vault.forEach(val => {
+		var name = val.name;
+		var username = val.username;
+		var password = val.password;
+		
+		const passwordElement = createPasswordElement(name, username, password);
+		PasswordList.prepend(passwordElement);
+		console.log(name)
+	})
+}
