7cb02a8a5b8bca8fd379fd6e0874e5bba814765d
Abhinav <abhinavkumr77@gmail.com>
2022-05-22 21:04:43 +0530

    update method added

--- app/credential.js
+++ app/credential.js
@@ -6,9 +6,9 @@ const namee = document.querySelector('#name');
 const username = document.querySelector('#username'); 
 const password = document.querySelector('#password'); 
 const closeNow = document.querySelector('#close-btn');
- 
+const id = document.getElementById('id');
 addToVault.addEventListener('click', (event) => {
-	mainProcess.addCredentials(namee.value,username.value,password.value, currentWindow);
+	mainProcess.addCredentials(id.value, namee.value,username.value,password.value, currentWindow);
 	mainProcess.getAllData();
 	namee.value = '';
 	username.value = '';
@@ -17,6 +17,15 @@ addToVault.addEventListener('click', (event) => {
 
 closeNow.addEventListener('click', (event) => {
 	mainProcess.closeWindow(currentWindow);
+});
 
-})
 
+ipcRenderer.on('update-method', (event, value) =>{
+	addToVault.innerText = 'Update';
+	document.querySelector('title').textContent = 'Update Credentials';
+	console.log(value)
+	id.value = value.id;
+	namee.value = value.name;
+	username.value = value.username;
+	password.value = value.password;
+});
