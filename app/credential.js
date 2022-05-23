812100f7168d17efcbf33452048f089f86018feb
Abhinav <abhinavkumr77@gmail.com>
2022-05-23 15:29:09 +0530

    validate data

--- app/credential.js
+++ app/credential.js
@@ -2,30 +2,37 @@ const {ipcRenderer} = require('electron');
 const mainProcess = require('@electron/remote').require('./main.js');
 const currentWindow = require('@electron/remote').getCurrentWindow();
 const addToVault = document.querySelector('#add-to-vault');
-const namee = document.querySelector('#name'); 
+const name = document.querySelector('#name'); 
 const username = document.querySelector('#username'); 
 const password = document.querySelector('#password'); 
 const closeNow = document.querySelector('#close-btn');
 const id = document.getElementById('id');
+
 addToVault.addEventListener('click', (event) => {
-	mainProcess.addCredentials(id.value, namee.value,username.value,password.value, currentWindow);
-	mainProcess.getAllData();
-	namee.value = '';
-	username.value = '';
-	password.value = '';
+	namee = name.value.trim();
+	usernamee = username.value.trim();
+	passwordd = password.value.trim();
+
+	if(namee && usernamee && passwordd ){
+		console.log(namee, usernamee, passwordd);
+		mainProcess.addCredentials(id.value, namee, usernamee, passwordd, currentWindow);
+		mainProcess.getAllData();
+		name.value = '';
+		username.value = '';
+		password.value = '';
+	}
 });
 
 closeNow.addEventListener('click', (event) => {
 	mainProcess.closeWindow(currentWindow);
 });
 
-
 ipcRenderer.on('update-method', (event, value) =>{
 	addToVault.innerText = 'Update';
 	document.querySelector('title').textContent = 'Update Credentials';
 	console.log(value)
 	id.value = value.id;
-	namee.value = value.name;
+	name.value = value.name;
 	username.value = value.username;
 	password.value = value.password;
 });
